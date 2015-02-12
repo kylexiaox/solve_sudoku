2794e34bd5f8d653fb5b5b027f7ebb0f184dad31
xiang xiao <btxiaox@gmail.com>
2015-02-13 00:02:32 +0800

    make some progess, to be continue...

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,5 @@
+
+
 __author__ = 'kyle_xiao'
 
 import math
@@ -34,6 +36,11 @@ class Cell(object):
         else:
             self.cell = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+class status(object):
+    def __init__(self,map,row,col):
+        self.map = map
+        self.row = row
+        self.col = col
 
 class SolveSudoku(object):
     _map = [[Cell for col in range(9)] for row in range(9)]
@@ -41,12 +48,16 @@ class SolveSudoku(object):
     _setCol = [set() for col in range(9)]
     _setCell = [[set() for col in range(3)] for row in range(3)]
     _step_number = 0
+    _isBottleneck = False
+    _lastStableMap = None
+    _statusStack = []
 
     def __init__(self, map):
         for row in range(9):
             for col in range(9):
                 self._map[row][col] = Cell(map[row][col])
         self.flushSet()
+        self._lastMap = self._map
 
 
     def flushSet(self):
@@ -62,7 +73,7 @@ class SolveSudoku(object):
         self._step_number = self._step_number + 1
         print("flush Set, Setp: " + str(self._step_number) + "\n")
         self.printMap()
-        csv.writeFromArray('middle.csv',self._map)
+        csv.writeFromArray('middle_3.csv', self._map)
         return over
 
 
@@ -70,28 +81,56 @@ class SolveSudoku(object):
         for row in range(9):
             for col in range(9):
                 if self._map[row][col].number == None:
+                    # if len(self._map[row][col].cell) == 0:
+                    #     recoverStatus = self._statusStack.pop()
+                    #     self._map = recoverStatus.map
+                    #     self._map[recoverStatus.row][recoverStatus.col].number = self._map[recoverStatus.row][recoverStatus.col].cell.pop()
+                    #     self.flushSet()
+                    #     return
                     self._map[row][col].cell = self._map[row][col].cell - self._setRow[row] - \
                                                self._setCell[int(math.ceil(row / 3))][int(math.ceil(col / 3))] - \
                                                self._setCol[col]
                     if len(self._map[row][col].cell) == 1:
                         self._map[row][col].number = self._map[row][col].cell.pop()
+                    # elif self._isBottleneck == True and len(self._map[row][col].cell) == 2:
+                    #     self._map[row][col].number = self._map[row][col].cell.pop()
+                    #     currentStatus = status(self._map,row,col)
+                    #     self._statusStack.append(currentStatus)
+                    #     self._isBottleneck = False
                     else:
-                        temp = []
                         # row
+                        temp = self._map[row][col].cell
                         for i in range(9):
-                            if self._map[row][i].number==None and i!= col:
-                                temp = self._map[row][col].cell - self._map[row][i].cell
+                            if self._map[row][i].number == None and i != col:
+                                temp = temp - self._map[row][i].cell
                         if len(temp) == 1:
                             self._map[row][col].number = temp.pop()
                             self._map[row][col].cell.clear()
+                            continue
                         # col
+                        temp = self._map[row][col].cell
                         for i in range(9):
-                            if self._map[j][none].number==None and i!= col:
-                                temp = self._map[row][col].cell - self._map[row][i].cell
+                            if self._map[i][col].number == None and i != col:
+                                temp = temp - self._map[i][col].cell
                         if len(temp) == 1:
                             self._map[row][col].number = temp.pop()
                             self._map[row][col].cell.clear()
+                            continue
+                        # cell
+                        temp = self._map[row][col].cell
+                        rowIndex = int(math.ceil(row / 3))
+                        colIndex = int(math.ceil(col / 3))
+                        for i in range(rowIndex, rowIndex + 3):
+                            for j in range(colIndex, colIndex):
+                                temp = temp - self._map[i][j].cell
+                        if len(temp) == 1:
+                            self._map[row][col].number = temp.pop()
+                            self._map[row][col].cell.clear()
+                            continue
+        if not cmp(self._map, self._lastMap):
+            self._isBottleneck = True
 
+        self._lastMap = self._map
 
     def solveSudoku(self):
         while not self.flushSet():
@@ -109,7 +148,10 @@ class SolveSudoku(object):
         output = ''
         for row in range(len(self._map)):
             for col in range(len(self._map[row])):
-                output = output + ', ' + str(self._map[row][col].number)
+                if self._map[row][col].number != None:
+                    output = output + ', ' + str(self._map[row][col].number)
+                else:
+                    output = output + ', 0'
             output = output + '\n'
         print(output)
 
